7e68a18f6b706444e5b9b8c7bcfaadea6da52fb7
HaanimP <haanimpietersen@gmail.com>
2024-06-12 15:08:30 +0200

    Fix version compatibility between Flask and Werkzeug

--- backend/app.py
+++ backend/app.py
@@ -58,4 +58,6 @@ def get_weather():
         return jsonify({'error': 'An error occurred while fetching weather data'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
